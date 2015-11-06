15c4314e84e3c5cab75337e0a8d02284c2d5d8b8
Diego Giovane Pasqualin <dpasqualin@gmail.com>
2015-11-06 18:47:49 -0200

    Standardize tip_labels type

--- R/sfreemap.map.R
+++ R/sfreemap.map.R
@@ -54,7 +54,7 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
     }
 
     if (all(inherits(prior, "list"), !inherits(Q, "list"))) {
-        stop ("if 'prior' is a list 'Q' should be a list with of same size.")
+        stop("if 'prior' is a list 'Q' should be a list with of same size.")
     }
 
     if (all(inherits(prior, "list"), inherits(Q, "list"), length(prior) != length(Q))) {
@@ -72,7 +72,7 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
             tree <- tree[[idx]]
         }
 
-        if (all(type == "dna", inherits(tip_states, "matrix"), ncol(tip_states) > 1)) {
+        if (all(inherits(tip_states, "matrix"), ncol(tip_states) > 1)) {
             tip_states <- tip_states[,idx]
         }
 
@@ -83,7 +83,6 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
         if (inherits(prior, "list")) {
             prior <- prior[[idx]]
         }
-
 
         params <- list(
             "tree" <- tree
@@ -136,7 +135,7 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
     } else if (inherits(prior, "list")) {
         # we can run multiple priors on trees and Q rate matrices
         return(serial_or_parallel(length(prior), tree, tip_states, Q, prior))
-    } else if (all(type == "dna", inherits(tip_states, "matrix"), ncol(tip_states) > 1)) {
+    } else if (all(inherits(tip_states, "matrix"), ncol(tip_states) > 1)) {
         # if single tree but multiple tip_states (dna type), call sfreemap.map
         # for each set of tip label
         return(serial_or_parallel(ncol(tip_states), tree, tip_states, Q, prior))
@@ -191,22 +190,21 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
     # The problem is that for function Q_dna the tip_states should not be a
     # matrix, but a character vector instead. For other situtations it should be
     # a matrix. One option is to adapt Q_dna to work with a matrix..
-    check_tip_states <- function(Q) {
-        possible_states <- NULL
-        if (!is.matrix(tip_states)) {
-            if (all(type == "dna", !is.null(Q), is.matrix(Q))) {
-                possible_states <- rownames(Q)
-            }
-            tip_states <- build_states_matrix(tree$tip.label, tip_states, possible_states)
+    check_tip_states <- function() {
+        if (type == "dna") {
+            possible_states <- c("a", "c", "t", "g", "-")
+        } else {
+            possible_states <- NULL
         }
+        tip_states <- build_states_matrix(tree$tip.label, tip_states, possible_states)
         return(tip_states)
     }
 
     if (all(!is.null(Q), is.matrix(Q))) {
-        tip_states <- check_tip_states(Q)
+        tip_states <- check_tip_states()
         QP <- Q_matrix(tree, tip_states, Q, model, prior, tol)
     } else if (type == 'standard') {
-        tip_states <- check_tip_states(Q)
+        tip_states <- check_tip_states()
         # standard data type has currently two ways of estimating the rate
         # matrix
         if (method == "empirical") {
@@ -222,7 +220,7 @@ sfreemap.map <- function(tree, tip_states, Q=NULL, type="standard", model="SYM",
     # Estimating Q when using nucleotide data
     } else if (all(type == "dna", is.null(Q))) {
         QP <- Q_dna(tip_states, tree, model, tol)
-        tip_states <- check_tip_states(QP$Q)
+        tip_states <- check_tip_states()
     }
 
     # Set the final value
