959f01d12cbcebf3dac09b1ffe4250f0f8384e61
Diego Giovane Pasqualin <dpasqualin@gmail.com>
2016-01-11 18:50:26 -0200

    Rename sfreemap.map to sfreemap

--- tests/testthat/test-sfreemap.map-multiPhylo.R
+++ tests/testthat/test-sfreemap.map-multiPhylo.R
@@ -1,8 +1,8 @@
-context('sfreemap.map')
+context('sfreemap')
 
 test_that ('works for multiphylo, standard, empirical', {
 
-    sm <- sfreemap.map(sfreemap.corals.trees[1:10], sfreemap.corals.tips, type="standard", method="empirical")
+    sm <- sfreemap(sfreemap.corals.trees[1:10], sfreemap.corals.tips, type="standard", method="empirical")
 
     expect_true(inherits(sm, "multiPhylo"))
     expect_equal(length(sm), 10)
@@ -14,9 +14,9 @@ test_that ('works for multiphylo, standard, empirical', {
 
 test_that ('works for multiphylo, standard, empirical, Q given', {
 
-    sm1 <- sfreemap.map(sfreemap.corals.trees[1:10], sfreemap.corals.tips, type="standard", method="empirical")
+    sm1 <- sfreemap(sfreemap.corals.trees[1:10], sfreemap.corals.tips, type="standard", method="empirical")
     Q <- lapply(sm1, function(x) x$Q)
-    sm2 <- sfreemap.map(sfreemap.corals.trees[1:10], sfreemap.corals.tips, Q=Q, type="standard", method="empirical")
+    sm2 <- sfreemap(sfreemap.corals.trees[1:10], sfreemap.corals.tips, Q=Q, type="standard", method="empirical")
 
     expect_true(inherits(sm1, "multiPhylo"))
     expect_equal(length(sm1), 10)
@@ -25,7 +25,7 @@ test_that ('works for multiphylo, standard, empirical, Q given', {
 
 test_that ('works for multiphylo, standard, mcmc', {
 
-    sm <- sfreemap.map(sfreemap.corals.trees[1:5], sfreemap.corals.tips, type="standard", method="mcmc", n_simulation=10)
+    sm <- sfreemap(sfreemap.corals.trees[1:5], sfreemap.corals.tips, type="standard", method="mcmc", n_simulation=10)
 
     expect_true(inherits(sm, "multiPhylo"))
     expect_equal(length(sm), 50) # 5 trees X 10 simulations
@@ -37,9 +37,9 @@ test_that ('works for multiphylo, standard, mcmc', {
 
 test_that ('works for multiphylo, standard, mcmc, Q given', {
 
-    sm1 <- sfreemap.map(sfreemap.corals.trees[1:5], sfreemap.corals.tips, type="standard", method="mcmc", n_simulation=5)
+    sm1 <- sfreemap(sfreemap.corals.trees[1:5], sfreemap.corals.tips, type="standard", method="mcmc", n_simulation=5)
     Q <- lapply(sm1, function(x) x$Q)
-    sm2 <- sfreemap.map(sfreemap.corals.trees[1:25], sfreemap.corals.tips, Q=Q, type="standard", method="mcmc", n_simulation=5)
+    sm2 <- sfreemap(sfreemap.corals.trees[1:25], sfreemap.corals.tips, Q=Q, type="standard", method="mcmc", n_simulation=5)
 
     expect_true(inherits(sm1, "multiPhylo"))
     expect_equal(length(sm1), 25) # 5 trees X 5 simulations
