0ad9d8d1ba3e7fac4993e4bb40a86fcaac084b1b
Diego Giovane Pasqualin <dpasqualin@gmail.com>
2016-02-21 23:17:09 -0300

    atualiza defesa.odp

--- R/plot_mutation_rate.R
+++ R/plot_mutation_rate.R
@@ -3,15 +3,22 @@ plot_mutation_rate <- function(tree, combination=3) {
         stop("tree must be a 'multiPhylo' object")
     }
     if (!is.integer(length(tree))/combination) {
-        stop("number of trees must be a multiple of 'combinatio'")
+        stop("number of trees must be a multiple of 'combination'")
     }
+
 
     groups <- list()
     for (i in 1:combination) {
         # all trees of the ith group
         group <- tree[seq(i, length(tree), combination)]
         # mutation rate for every branch on every tree of the group
-        groups[[i]] <- c(sapply(group, function(t) rowSums(t$mapped.edge.lmt)/t$edge.length))
+        group <- c(sapply(group, function(t) rowSums(t$mapped.edge.lmt)/t$edge.length))
+        # remove outliers
+        group <- group[group <= boxplot.stats(group)$stats[5]]
+        # make sure all groups have the same number of values
+        group <- c(group, rep(NA, number_of_edges-length(group)))
+        # save
+        groups[[i]] <- group
     }
     groups <- sapply(groups, cbind)
     colnames(groups) <- 1:combination
@@ -20,7 +27,9 @@ plot_mutation_rate <- function(tree, combination=3) {
     colnames(to_plot) <- c('n', 'color', 'value')
     to_plot$color <- as.character(to_plot$color)
 
-    p <- ggplot(to_plot, aes(x=value, fill=color)) + geom_histogram()
+    p <- ggplot(to_plot, aes(x=value, fill=color)) +
+            geom_density(position='stack', na.rm=TRUE)
+
     print(p)
 
     return(data.frame(to_plot))
